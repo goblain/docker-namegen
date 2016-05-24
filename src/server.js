97628dd2cc9e8f917348d8c854c71698206e9f7e
Radek Pieczonka <goblin@pentex.pl>
2016-05-24 15:29:22 +0200

    return json with more info

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+var os = require('os');
 
 app.get('/', function(req, res) {
   var NameGenerator = require("name_generator");
-  res.type('text/plain');
-  res.send(NameGenerator());
+  res.type('application/json');
+  res.send(JSON.stringify({ name: NameGenerator(), version: 1, hostname: os.hostname() }));
 });
 
 app.listen(80);
