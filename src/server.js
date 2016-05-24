165565f7501dd12e8f74d051981e19072b30a571
Radek Pieczonka <goblin@pentex.pl>
2016-05-24 15:33:21 +0200

    uppercase v2

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ var os = require('os');
 app.get('/', function(req, res) {
   var NameGenerator = require("name_generator");
   res.type('application/json');
-  res.send(JSON.stringify({ name: NameGenerator(), version: 1, hostname: os.hostname() }));
+  res.send(JSON.stringify({ name: NameGenerator().toUpperCase(), version: 2, hostname: os.hostname() }));
 });
 
 app.listen(80);
